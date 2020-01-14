23d36e6f4e3911728000b962f9d2cb254e78867d
Alex <alexxsur@gmail.com>
2020-01-13 19:16:45 -0500

    Actualizar Paquetes

--- server/server.js
+++ server/server.js
@@ -14,10 +14,11 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos ONLINE');
-});
+mongoose.connect('mongodb://localhost:27017/cafe', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
+    });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
