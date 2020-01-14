76cdf3079d17857b94cb7361375420d3e35c4cfa
Alex <alexxsur@gmail.com>
2020-01-13 20:56:10 -0500

    Variable de entorno MONGO_URI Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mongouser:83sdPpXKBlC2kVR3@cluster0-wh1la.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
