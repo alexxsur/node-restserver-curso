4a6164375084d13a5c467ee4908c295c28efd7ea
Alex <alexxsur@gmail.com>
2020-01-13 20:35:17 -0500

    Cadena de conexion mongo dev y prod

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+mongoose.connect(process.env.URLDB, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
     (err, res) => {
         if (err) throw err;
         console.log('Base de datos ONLINE');
