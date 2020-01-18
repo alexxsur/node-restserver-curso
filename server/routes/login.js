377e2c84056aec87ef2d968183408adb0f3d471a
Alex <alexxsur@gmail.com>
2020-01-18 16:50:23 -0500

    Generar un JWT

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,14 +1,51 @@
 const express = require('express');
+
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
 const app = express();
 
 app.post('/login', function(req, res) {
+    let body = req.body;
 
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
 
-    res.json({
-        ok: true,
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: '(Usuario) o contraseña incorrectos'
+                }
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o (contraseña) incorrectos'
+                }
+            });
+
+            let token = jwt.sign({
+                usuario: usuarioDB
+            }, 'este-es-el-seed-desarrollo', { expiresIn: 60 * 60 * 24 * 30 });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token: '123'
+        });
     });
 });
 
