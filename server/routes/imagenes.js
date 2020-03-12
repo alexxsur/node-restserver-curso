66fe9440de07519d585f00fe07362b8cc4286fd7
Alex <alexxsur@gmail.com>
2020-03-11 20:22:52 -0500

    Servicio para mostrar las imagenes

---
+++ server/routes/imagenes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+
+let app = express();
+
+app.get('/imagen/:tipo/:img', (req, res) => {
+
+    let tipo = req.params.tipo;
+    let img = req.params.img;
+
+    let pathImg = `./uploads/${ tipo }/${ tipo }`;
+
+    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+
+    res.sendFile(noImagePath);
+
+})
+
+
+
+module.exports = app;
